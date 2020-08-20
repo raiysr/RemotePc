e7ecc65a6f75b4504874c288f7d87837f81a20b0
raiysr <140647@qq.com>
2020-08-20 12:19:28 +0800

    新增KEYHOOKDLL 20200820

---
+++ KeyHook/KeyHook/dllmain.cpp
@@ -0,0 +1,84 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+
+HHOOK g_hhk;
+HMODULE g_hModule;
+HWND g_hWnd;
+
+LRESULT CALLBACK KeyBoardProc
+(
+	int nCode,
+	WPARAM wParam,
+	LPARAM lParam
+)
+{
+	unsigned int nVKCode = wParam;
+	char szBuf[256];
+	if (nVKCode >= 'A' && nVKCode <= 'Z')
+	{
+		sprintf_s(szBuf, "%c pressed", nVKCode);
+		/*OutputDebugStringA(szBuf);*/
+
+		COPYDATASTRUCT cds;
+		cds.dwData = 0;
+		cds.cbData = strlen(szBuf) + 1;//要传送的数据长度
+		cds.lpData = szBuf;				//要传送的数据
+
+		SendMessage(g_hWnd,   //想改窗口句柄发送消息
+			WM_COPYDATA,
+			(WPARAM)g_hWnd,   //表示当前自己的窗口句柄,可以不写
+			(LPARAM)&cds      //构建一个结构体,用于将数据传输
+			);
+
+	}
+
+	return CallNextHookEx(g_hhk, nCode, wParam, lParam);
+}
+
+//dll 导出函数,提供给其他程序使用
+BOOL MySetHook(HWND hWnd)
+{
+	g_hWnd = hWnd;
+	g_hhk = SetWindowsHookEx(WH_KEYBOARD,
+		(HOOKPROC)KeyBoardProc,
+		g_hModule, //表示第三方的注入dll ,全局钩子使用,DLL的模块句柄
+		0 //线程ID,填0 表示勾所有线程
+	);
+	if (g_hhk == NULL)
+	{
+		return FALSE;
+	}
+
+	return TRUE;
+
+}
+
+
+
+
+BOOL APIENTRY DllMain(HMODULE hModule,
+	DWORD  ul_reason_for_call,
+	LPVOID lpReserved
+)
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	{
+		g_hModule = hModule;
+		//当DLL被加载时调用
+		//编写初始化操作
+	}
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+	{
+		//当DLL被释放时调用
+		//编写反初始化操作
+	}
+	break;
+	}
+	return TRUE;
+}
+
--- del.bat
+++ del.bat
@@ -1,4 +1,85 @@
 @echo off
-for /r @@i in (debug,release,ipch) do rd /s /q "@@i"
-for /r @@i in (*.sdf *.VC.db) do del /s /f /q "@@i"
+echo 正在清除项目临时文件，请稍等......
+
+del /f /s /q %~dp0\*.bin
+del /f /s /q %~dp0\*.ncb
+del /f /s /q %~dp0\*.user
+del /f /s /q %~dp0\*.pch
+del /f /s /q %~dp0\*.obj
+del /f /s /q %~dp0\*.tlh
+del /f /s /q %~dp0\*.tli
+del /f /s /q %~dp0\*.res
+del /f /s /q %~dp0\*.idb
+del /f /s /q %~dp0\*.pdb
+del /f /s /q %~dp0\*.aps
+del /f /s /q %~dp0\*.manifest
+del /f /s /q %~dp0\*.dep
+del /f /s /q %~dp0\*.log
+del /f /s /q %~dp0\*.txt
+del /f /s /q %~dp0\*.cjstyles
+del /f /s /q %~dp0\*.hwl
+del /f /s /q %~dp0\*.ilk
+del /f /s /q %~dp0\*.rar
+del /f /s /q %~dp0\*.zip
+del /f /s /q %~dp0\*.exe
+del /f /s /q %~dp0\*.dll
+del /f /s /q %~dp0\*.SRDB
+del /f /s /q %~dp0\*.SRDB.Bakup
+del /f /s /q %~dp0\*.jar
+del /f /s /q %~dp0\*.db
+del /f /s /q %~dp0\*.png
+del /f /s /q %~dp0\UpgradeLog.XML
+del /f /s /q %~dp0\UpgradeLog.XML
+del /f /s /q %~dp0\*.sdf
+del /f /s /q %~dp0\*.opensdf
+del /f /s /q %~dp0\*.i
+del /f /s /q %~dp0\*.clw
+del /f /s /q %~dp0\*.opt
+del /f /s /q %~dp0\*.plg
+del /f /s /q %~dp0\*.tlog
+del /f /s /q %~dp0\*.recipe
+del /f /s /q %~dp0\*.lastbuildstate
+
+
+
+
+if exist _UpgradeReport_Files (
+    cd _UpgradeReport_Files
+echo 当前目录[%cd%]
+del /f /s /q *.*
+    cd..
+rd _UpgradeReport_Files
+echo del _UpgradeReport_Files
+
+
+)
+
+
+
+
+if exist Release (
+    cd Release 
+echo 准备删除 Release 目录下的所有文件,请确认一下当前目录是否正确.再继续
+echo 当前目录[%cd%]
+del /f /s /q *.*
+    cd..
+rd Release 
+echo del Release 
+)
+
+
+
+
+if exist Debug (
+    cd Debug
+echo 当前目录[%cd%]
+del /f /s /q *.*
+    cd..
+rd Debug
+echo del Debug
+)
+
+
+
+
 pause
